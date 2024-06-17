16e2b4ee6f78216f7445c197c8d14b3bdfde6f60
Isa Abdullah Juthani <bjuthani1989@gmail.com>
2024-06-17 23:04:56 +0530

    created models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.timezone import now
 
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    item_name = models.CharField(max_length=64)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    created_date = models.DateTimeField(default=now)
+    item_price = models.DecimalField(max_digits=10,decimal_places=2)
+
+
+class Bid(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
+
