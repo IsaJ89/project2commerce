f7ef2ffcef9fa6df89a56639716280c6c9265052
Isa Abdullah Juthani <bjuthani1989@gmail.com>
2024-06-19 11:12:36 +0530

    modified model listing

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -3,7 +3,7 @@ from .models import User, Listing
 
 # Register your models here.
 class ListingAdmin(admin.ModelAdmin):
-    list_display = ("id", "item_name", "created_by", "created_date", "initial_bid")
+    list_display = ("id", "item_name", "created_by", "created_date", "description", "initial_bid")
 
 admin.site.register(User)
 admin.site.register(Listing, ListingAdmin)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -12,6 +12,8 @@ class Listing(models.Model):
     created_date = models.DateTimeField(default=now)
     description = models.TextField(max_length=300, default="Description missing")
     initial_bid = models.DecimalField(max_digits=10,decimal_places=2)
+    photo = models.TextField(max_length=150, default="Image link missing")
+    category = models.CharField(max_length=50, default="Category missing")
 
     def __str__(self):
         return f"{self.item_name} {self.created_by.username} {self.created_date} {self.initial_bid}"
