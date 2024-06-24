578d977f69464cec7b6a6fe5e091428a4769d889
Isa Abdullah Juthani <bjuthani1989@gmail.com>
2024-06-24 08:11:22 +0530

    still working on bids

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from .forms import ListingForm, BidForm
-from .models import User, Listing
+from .models import User, Listing, Bid
 
 
 def index(request):
@@ -69,21 +69,43 @@ def create_listing(request):
     if request.method == "POST":
         form = ListingForm(request.POST, request.FILES)
         if form.is_valid():
-            listing = form.save(commit=False)
+            listing = form.save(commit=False) # commit=False prevents the data from being saved to the database immediately
             listing.created_by = request.user
             listing.created_date = now()
             listing.save()
             return HttpResponseRedirect(reverse("index"))
     else:
         form = ListingForm()
-    return render(request, "auctions/listing.html", {
+    return render(request, "auctions/create_listing.html", {
     'form': form
 })
 
 def view_listing(request, listing_id):
+    bid_form = BidForm()
     listing = Listing.objects.get(pk=listing_id)
+    bids = listing.bids.all() # get bids associated with the listing
     if request.method == "POST":
-        pass
+        bid_form = BidForm(request.POST)
+        if bid_form.is_valid():
+            bid_value = bid_form.cleaned_data['bid_value']
+        
+            if bids.bid_value:
+                for bid in bids.bid_value:
+                    if bid_value < bid:
+                        return render(request, "auctions/view_listing.html",{
+                            "message": "Sorry, bid must be higher than the current price of the item"
+                        })
+            else:
+                # check if bid_value is lesser than starting_bid
+                if bid_value < listing.starting_bid:
+                    return render(request, "auctions/view_listing.html",{
+                        "message": "Sorry! Your bid must be higher than the starting bid"
+                    })
+            
+
+        
+
     return render(request, "auctions/view_listing.html", {
-        "listing": listing
+        "listing": listing,
+        "bid_form": bid_form
     })
