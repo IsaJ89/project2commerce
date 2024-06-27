e29f9451f2f6f8fdfc823c4248170ae8ef1ea967
Isa Abdullah Juthani <bjuthani1989@gmail.com>
2024-06-27 18:07:54 +0530

    made models more editable in admin

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -4,16 +4,18 @@ from .models import *
 # Register your models here.
 class ListingAdmin(admin.ModelAdmin):
     list_display = ("id", "item", "created_by", "created_date", "status", "description", "starting_bid", "image", "category")
-    fields = ("status",)
+    fields = ("id", "item", "created_by", "created_date", "status", "description", "starting_bid", "image", "category")
 
 class BidAdmin(admin.ModelAdmin):
     list_display = ("id", "item", "placed_by", "bid_value", "placed_on")
+    fields = ("id", "item", "placed_by", "bid_value", "placed_on")
 
 class WatchlistAdmin(admin.ModelAdmin):
     list_display = ("id", "item" , "user")
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ("item", "comment", "made_by", "made_on")
+    fields = ("item", "comment", "made_by", "made_on")
 
 
 
