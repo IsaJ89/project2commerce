30dec20c5686b7d16a57dfa64401bf9b6138fd54
Isa Abdullah Juthani <bjuthani1989@gmail.com>
2024-06-27 18:09:44 +0530

    fixed some errors

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -4,11 +4,11 @@ from .models import *
 # Register your models here.
 class ListingAdmin(admin.ModelAdmin):
     list_display = ("id", "item", "created_by", "created_date", "status", "description", "starting_bid", "image", "category")
-    fields = ("id", "item", "created_by", "created_date", "status", "description", "starting_bid", "image", "category")
+    fields = ("item", "created_by", "created_date", "status", "description", "starting_bid", "image", "category")
 
 class BidAdmin(admin.ModelAdmin):
     list_display = ("id", "item", "placed_by", "bid_value", "placed_on")
-    fields = ("id", "item", "placed_by", "bid_value", "placed_on")
+    fields = ("item", "placed_by", "bid_value", "placed_on")
 
 class WatchlistAdmin(admin.ModelAdmin):
     list_display = ("id", "item" , "user")
